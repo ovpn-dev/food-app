0b96f4cdc19d0b9c7a4de8a2aa5a1460991eaee5
ovpn-dev <ovpn05@gmail.com>
2025-07-07 13:50:43 +0100

    homescreen done not dynamic

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,38 @@
-import { offers } from "@/constants";
+import CartButton from "@/components/CartButton";
+import { images, offers } from "@/constants";
+import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
+          const isEven: boolean = index % 2 === 0;
+
           return (
             <View>
               <Pressable
-                className="offer-card"
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
                 style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#ffffff22" }}
               >
                 {({ pressed }) => (
                   <Fragment>
-                    <View className="h-full w-1/2">
+                    <View className={"h-full w-1/2"}>
                       <Image
                         source={item.image}
                         className={"size-full"}
@@ -25,8 +40,21 @@ export default function Index() {
                       />
                     </View>
 
-                    <View className={"offer-card__info"}>
-                      <Text>{item.title}</Text>
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor={"#fff"}
+                      />
                     </View>
                   </Fragment>
                 )}
@@ -34,6 +62,24 @@ export default function Index() {
             </View>
           );
         }}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-row flex-between w-full px-5 my-5">
+            <View>
+              <Text className="small-bold text-primary">Deliver to</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="p-bold text-black-100">Your Location</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <CartButton />
+          </View>
+        )}
       />
     </SafeAreaView>
   );
